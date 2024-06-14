9f4033a39a1ca8ca47db2f2d3e2a4a76c777f385
Samuel <furnish.samuel@gmail.com>
2024-06-13 23:41:15 -0500

    front page design complete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
+import CategoryMenu from "./components/category-menu/category-menu";
+
 
 const App = () => {
 
   const categories = [
     {
-      id: 1,
-      title: 'Hats',
+      "id": 1,
+      "title": "Hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'Jackets',
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 3,
-      title: 'Sneakers',
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 4,
-      title: "Women's",
+      "id": 4,
+      "title": "Women's",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 5,
-      title: "Men's",
-    },
+      "id": 5,
+      "title": "Men's",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ];
 
   return (
-    <div className='categories-container'>
-
-      {categories.map(({title, id}) => 
-        <div key={id} className='category-container' >
-          <div className='background-image' />
-          <div className='category-body-container'></div>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-        </div>
-       )}
-      </div>
+    <CategoryMenu categories={categories} />
   );
 };
 
