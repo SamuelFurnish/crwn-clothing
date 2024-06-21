909f04810f383d682c5c73e26329aabaebb788d9
Samuel <furnish.samuel@gmail.com>
2024-06-21 10:15:01 -0500

    checkout page update

--- src/routes/checkout/checkout.jsx
+++ src/routes/checkout/checkout.jsx
@@ -1,10 +1,29 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart';
+
 import './checkout.scss';
 
 const Checkout = () => {
+    const { cartItems } = useContext(CartContext);
+
 
     return (
         <div>
             <h1>I am the Checkout Page</h1>
+            <div>
+                {
+                    cartItems.map((cartItem) => {
+                        const {id, name, quantity} = cartItem;
+                        return (
+                            <div key={id}>
+                                <h2>{name}</h2>
+                                <span>{quantity}</span>
+                            </div>
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 };
