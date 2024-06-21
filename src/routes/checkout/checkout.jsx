f08451ab4f328a3aa7a05323d92eafb0f6e7bd86
Samuel <furnish.samuel@gmail.com>
2024-06-21 10:01:42 -0500

    added checkout page

---
+++ src/routes/checkout/checkout.jsx
@@ -0,0 +1,12 @@
+import './checkout.scss';
+
+const Checkout = () => {
+
+    return (
+        <div>
+            <h1>I am the Checkout Page</h1>
+        </div>
+    );
+};
+
+export default Checkout;
